4ce844db68925f3277157bd82377e3d1bc99c67d
Saifullah Mahmud Sumon <sumonsmailbox@gmail.com>
2018-09-17 09:31:17 +0600

    timer with historical list implemented

--- time-tracker-frontend/src/App.js
+++ time-tracker-frontend/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Spocket Timer</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+           <h1>Timer</h1>
+           <Timer status={false} runningTime={0} />
+        </div>
       </div>
     );
   }
---
+++ time-tracker-frontend/src/Timer.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  state = {
+    timerRecords: 0,
+    list: [],
+    started: false,
+    runningTime: 0
+  };
+
+  render () {
+    const { timerRecords, list, started, runningTime } = this.state;
+    const children = [];
+    for (var i = 0; i < this.state.list.length; i += 1) {
+      children.push(<HistoryLine key={i} item={this.state.list[i]}/>);
+    };
+    return (
+      <div>
+        <p>{Math.round(runningTime/1000)} seconds</p>
+        <HistoryList addChild={this.onAddChild} started={started} >
+          {children}
+        </HistoryList>
+      </div>
+    );
+  }
+
+  onAddChild = () => {
+    this.setState(state => {
+      if (state.started) {
+        const startTime = Date.now() - this.state.runningTime;
+        const record = this.state.list.concat(Math.round((Date.now() - startTime)/1000));
+        clearInterval(this.timer);
+        this.setState({
+          runningTime: 0,
+          started: false,
+          list: record
+        });
+
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({
+            runningTime: Date.now() - startTime
+          });
+        });
+      }
+      return { started: !state.started };
+    });
+  };
+
+  handleReset = () => {
+    clearInterval(this.timer);
+    this.setState({ runningTime: 0, started: false });
+  };
+}
+
+const HistoryList = props => (
+  <div>
+    <p><button onClick={props.addChild}>{props.started ? 'Stop' : 'Start'}</button></p>
+    <h3>{props.children.length > 0 ? 'History' : ''}</h3>
+    <div id="children-pane">
+      {props.children}
+    </div>
+  </div>
+);
+
+const HistoryLine = props => <div className='line'>{props.item} seconds</div>;
+
+export default Timer;
