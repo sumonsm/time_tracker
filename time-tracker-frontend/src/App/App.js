0e1cb16c606f393eb31dc3d376dd4b652b4182aa
Sumon SM <sumonsm00@gmail.com>
2018-09-20 16:34:26 +0600

    redux and router setup done.

--- time-tracker-frontend/src/App/App.js
+++ time-tracker-frontend/src/App/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+
 import './App.css';
 import Timer from '../Timer/Timer';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+
+      const { dispatch } = this.props;
+      history.listen((location, action) => {
+          // clear alert on location change
+          dispatch(alertActions.clear());
+      });
+  }
+
   render() {
+    const { alert } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Spocket Timer</h1>
         </header>
-        <div>
-           <h1>Timer</h1>
-           <Timer status={false} runningTime={0} />
-        </div>
+        <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                      <Router history={history}>
+                          <div>
+                             <h1>Timer</h1>
+                             <Timer status={false} runningTime={0} />
+                          </div>
+                      </Router>
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
 }
 
-export default App;
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export { connectedApp as App };
